c18778bd66f856b201692c0d1068503efaa3e223
MrCat <a976466014@gmail.com>
2024-07-24 22:40:01 +0800

    1

--- gradio_demo.py
+++ gradio_demo.py
@@ -2,7 +2,7 @@
 
 import os
 base_path = './Llama-3.1-8B-Instruct-BaJie-Chat'
-os.system(f'git clone https://code.openxlab.org.cn/MrCat/Llama-3.1-8B-Instruct-BaJie-Chat.git {base_path}')
+os.system(f'git clone https://www.modelscope.cn/JimmyMa99/BaJie-Chat-llama3_1-8b.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 os.system(f'cd .. && pip install lmdeploy')
 os.system(f'lmdeploy serve gradio {base_path} --model-name llama3 --server-port 7860')
