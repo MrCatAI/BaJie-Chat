bb9bd49d244216519cef6f26eb5354def41d26b9
MrCat <a976466014@gmail.com>
2024-07-24 23:52:36 +0800

    base_path

--- gradio_demo.py
+++ gradio_demo.py
@@ -1,10 +1,9 @@
 # 临时demo ,配置已更新，进去全部更新完毕！！
 
 import os
-base_path = './BaJie-Chat-llama3_1-8b'
+base_path = './JimmyMa99/BaJie-Chat-llama3_1-8b'
 #模型下载
 from modelscope import snapshot_download
 model_dir = snapshot_download('JimmyMa99/BaJie-Chat-llama3_1-8b',cache_dir='./')
-os.system(f'cd {base_path} && git lfs pull')
 os.system(f'cd .. && pip install lmdeploy')
 os.system(f'lmdeploy serve gradio {base_path} --model-name llama3 --server-port 7860')
