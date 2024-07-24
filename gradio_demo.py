1caaa5f14cd136ef03f4cadaab05db28d82af8b2
MrCat <a976466014@gmail.com>
2024-07-24 23:19:34 +0800

    modelscope

--- gradio_demo.py
+++ gradio_demo.py
@@ -2,8 +2,9 @@
 
 import os
 base_path = './Llama-3.1-8B-Instruct-BaJie-Chat'
-os.system(f'apt-get install git-lfs')
-os.system(f'git clone https://www.modelscope.cn/JimmyMa99/BaJie-Chat-llama3_1-8b.git {base_path}')
+#模型下载
+from modelscope import snapshot_download
+model_dir = snapshot_download('JimmyMa99/BaJie-Chat-llama3_1-8b',cache_dir='./')
 os.system(f'cd {base_path} && git lfs pull')
 os.system(f'cd .. && pip install lmdeploy')
 os.system(f'lmdeploy serve gradio {base_path} --model-name llama3 --server-port 7860')
