be77ff71f8634d009ddd3f3f482420c913b16ed4
MrCat <a976466014@gmail.com>
2024-07-24 17:12:25 +0800

    llama3.1 app

---
+++ gradio_demo.py
@@ -0,0 +1,8 @@
+# 临时demo ,配置已更新，进去全部更新完毕！！
+
+import os
+base_path = './Llama-3.1-8B-Instruct-BaJie-Chat'
+os.system(f'git clone https://code.openxlab.org.cn/MrCat/Llama-3.1-8B-Instruct-BaJie-Chat.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+os.system(f'cd .. && pip install lmdeploy')
+os.system(f'lmdeploy serve gradio {base_path} --model-name llama3 -–server-port 7860')
