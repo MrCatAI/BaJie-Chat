2ab8922977729d533200fe36f73a4f28745cb192
MrCat <a976466014@gmail.com>
2024-07-24 23:26:41 +0800

    BaJie-Chat-llama3_1-8b

--- gradio_demo.py
+++ gradio_demo.py
@@ -1,7 +1,7 @@
 # 临时demo ,配置已更新，进去全部更新完毕！！
 
 import os
-base_path = './Llama-3.1-8B-Instruct-BaJie-Chat'
+base_path = './BaJie-Chat-llama3_1-8b'
 #模型下载
 from modelscope import snapshot_download
 model_dir = snapshot_download('JimmyMa99/BaJie-Chat-llama3_1-8b',cache_dir='./')
