24123a00b82e29191d405742a44d7f6113d8f5ca
MrCat <a976466014@gmail.com>
2024-07-24 22:57:28 +0800

    git

--- gradio_demo.py
+++ gradio_demo.py
@@ -2,7 +2,7 @@
 
 import os
 base_path = './Llama-3.1-8B-Instruct-BaJie-Chat'
-os.system(f'apt install git-fls')
+os.system(f'apt-get install git-lfs')
 os.system(f'git clone https://www.modelscope.cn/JimmyMa99/BaJie-Chat-llama3_1-8b.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 os.system(f'cd .. && pip install lmdeploy')
